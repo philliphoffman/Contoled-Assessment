048a5bbac113dd5ee468adcb9c96691fa4d16a04
Phillip FurryFace Hoffman <philliphoffman@hoffman.com>
2014-06-18 13:07:11 +0100

    Update currency.py

--- currency.py
+++ currency.py
@@ -1,13 +1,42 @@
 #Currency 
 GBP = 1
 EUR = 2
-USD = 3
-JPY = 4
-
-Number = raw_input("What number of currency do you wish to change just the number not the currency.?")
-
-if Number.isalpha 
-    print "Not a Number"
+USD = 5
+JPY = 4 
+Currency = raw_input("Currency you wish to convert from (GBP, EUR, USD, JPY?")
+if Currency.isalpha: 
+    print "You did not input a correct Currency"
     break
-else currency = raw_input("What currency (GBP, JPY, USD, EUR)")
+Ammount =  raw_input("Ammount of currency you wish to convert e.g 2.55")
+to = raw_input("Currency converting to?")
+if Currency = GBP and to = EUR:
+    print
+if Currency = GBP and to = USD:
+    print
+if Currency = GBP and to = JPY:
+    print
+if Currency = GBP and to = GBP:
+    print 
+if Currency = USD and to = USD:
+    print 
+if Currency = USD and to = EUR:
+    print 
+if Currency = USD and to = JPY:
+    print 
+if Currency = EUR and to = EUR:
+    print 
+if Currency = EUR and to = GBP:
+    print 
+if Currency = EUR and to = JPY:
+    print 
+if Currency = EUR and to = USD:
+    print 
+if Currency = JPY and to = EUR:
+    print 
+if Currency = JPY and to = USD:
+    print 
+if Currency = JPY and to = GBP:
+    print 
+if Currency = JPY and to = JPY:
+    print 
 
